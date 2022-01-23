9e60ee31d007608280d3da9aa87f9c5e3382d802
Joaco <joacosaralegui@gmail.com>
2022-01-23 19:03:47 +0100

    YouTube embed width

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -1,52 +1,52 @@
 
 var documents = [{
     "id": 0,
-    "url": "https://joacosaralegui.ar/404.html",
+    "url": "http://localhost:4000/404.html",
     "title": "404",
     "body": "404 Page not found!Please use the search bar from the bottom left or visit our homepage! "
     }, {
     "id": 1,
-    "url": "https://joacosaralegui.ar/about",
+    "url": "http://localhost:4000/about",
     "title": "Memoirs, a free minimalist Jekyll blogging theme with modern design",
     "body": "This website is a demonstration to see Memoirs Jekyll theme in action. The theme is compatible with Github pages, in fact even this demo itself is created with Github Pages and hosted with Github.  Get Memoirs for Jekyll → "
     }, {
     "id": 2,
-    "url": "https://joacosaralegui.ar/contacto",
+    "url": "http://localhost:4000/contacto",
     "title": "Contacto",
     "body": "                                           Joaco Saralegui:         Acá empieza la primera mentira. Soy Ingeniero en Sistemas, hincha de Racing, amateur del arte y mochilero. Siempre estoy interesado en hacer amigos así que si algo de esto te gusto no dudes en escribirme.                                                                                                                                                                 "
     }, {
     "id": 3,
-    "url": "https://joacosaralegui.ar/categories",
+    "url": "http://localhost:4000/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 4,
-    "url": "https://joacosaralegui.ar/contact",
+    "url": "http://localhost:4000/contact",
     "title": "Contact",
     "body": "  Please send your message to Joaco. We will reply as soon as possible!   "
     }, {
     "id": 5,
-    "url": "https://joacosaralegui.ar/",
+    "url": "http://localhost:4000/",
     "title": "Home",
     "body": "                                                                                               Placeholder              :       Placeholder text for my first postMore text:                                                                     Escritos                            Recomendaciones                                                                                   Peliculas                                              21 Jan 2022        &lt;/span&gt;                        "
     }, {
     "id": 6,
-    "url": "https://joacosaralegui.ar/escritos/",
+    "url": "http://localhost:4000/escritos/",
     "title": "Escritos",
     "body": ""
     }, {
     "id": 7,
-    "url": "https://joacosaralegui.ar/recomendaciones/",
+    "url": "http://localhost:4000/recomendaciones/",
     "title": "Recomendaciones",
     "body": ""
     }, {
     "id": 8,
-    "url": "https://joacosaralegui.ar/robots.txt",
+    "url": "http://localhost:4000/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 9,
-    "url": "https://joacosaralegui.ar/first/",
+    "url": "http://localhost:4000/first/",
     "title": "Placeholder",
     "body": "2022/01/21 - Placeholder text for my first post More text "
     }];
